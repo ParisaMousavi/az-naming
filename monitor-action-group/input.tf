83669bc11af79f5646da7b7335ea07be423fa547
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-06-29 17:40:34 +0200

    extending with monitor-action-group

---
+++ monitor-action-group/input.tf
@@ -0,0 +1,21 @@
+variable "name" {
+  type = string
+}
+
+variable "stage" {
+  type    = string
+  default = ""
+}
+
+variable "assembly" {
+  type    = string
+  default = ""
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
