a0b7bb78d23988ac377d6a7918445060cc2e4823
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2023-05-15 20:10:09 +0200

    update

--- vm/output.tf
+++ vm/output.tf
@@ -1,3 +1,3 @@
 output "result" {
-  value = format("${var.prefix}-vm-%s", join("-", compact([var.name, var.stage, var.assembly, var.location_shortname])))
+  value = format("${var.prefix}-vm-%s", join("", compact([var.name, var.stage, var.assembly, var.location_shortname])))
 }
